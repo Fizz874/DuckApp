9ca932906aec835a542f337a87f95dc95a7d7eac
0Artur1 <artur.strz@interia.pl>
2025-12-14 02:16:00 +0100

    Add filtering

---
+++ UI/Converters/FilterModeToIconConverter.cs
@@ -0,0 +1,40 @@
+﻿using CORE;
+using Strzelecki_Baranowski.DuckApp.UI;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Strzelecki_Baranowski.DuckApp.UI
+{
+    public class FilterModeToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is FilterModeEnum mode)
+            {
+                return mode switch
+                {
+                    FilterModeEnum.Equal => "=",
+                    FilterModeEnum.NotEqual => "≠",
+                    FilterModeEnum.Greater => ">",
+                    FilterModeEnum.Less => "<",
+                    FilterModeEnum.GreaterOrEqual => "≥",
+                    FilterModeEnum.LessOrEqual => "≤",
+                    FilterModeEnum.Contains => "⊆", // Ikona "zawiera tekst"
+                    FilterModeEnum.NotContains => "⊈",
+                    _ => "?"
+                };
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
